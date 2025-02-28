1ffd11a013fec1d466cf22b5386f8e8819e25af4
4aykovski <kan89kan@gmail.com>
2025-02-28 16:59:05 +0300

    init

---
+++ src/pages/home/home-page.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { BaseButton } from '@/components/ui/button'
+</script>
+
+<template>
+  <div class="flex justify-center items-center">
+    <h1>home</h1>
+    <base-button>cliock</base-button>
+  </div>
+</template>
